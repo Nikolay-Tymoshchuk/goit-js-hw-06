611ba6585c0ad8cc0a9f7a182ce34d9d65ba89d5
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-03-27 17:06:45 +0300

    finish 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,31 @@
-const counterValue = 0
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+console.log(decrementBtn.dataset.action);
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+console.log(incrementBtn.dataset.action);
+const valueEL = document.querySelector('#value');
+
+
+const counterValue = {
+    value: 0,
+    increment () {
+        this.value += 1;
+    },
+    decrement () {
+        this.value -= 1;
+    }
+};
+
+decrementBtn.addEventListener('click', () => { 
+    counterValue.decrement();
+    console.log('counterValue :>> ', counterValue.value);
+    valueEL.textContent = counterValue.value;
+}
+);
+
+incrementBtn.addEventListener('click', () => {
+    counterValue.increment();
+    console.log('counterValue :>> ', counterValue.value);
+    valueEL.textContent = counterValue.value;
+});
+
+
